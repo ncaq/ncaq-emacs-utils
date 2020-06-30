48cc4990ff4979ac67b6845a69b1b91352845af3
ncaq <ncaq@ncaq.net>
2020-06-30 15:17:06 +0900

    cleaned: 雑に治せる範囲の形式修正

--- ncaq-emacs-utils.el
+++ ncaq-emacs-utils.el
@@ -7,18 +7,19 @@
 (fset 'newline-upper [home return up])
 
 (defun quoted-newline ()
+  "強制的に改行を入力."
   (interactive)
   (insert "\n"))
 
 (defun smart-move-beginning-of-line ()
-  "Visual StudioライクなC-a, 通常はインデントに従い, 後ろが空白のみなら先頭"
+  "Visual Studioライクなmove-beginning-of-line, 通常はインデントに従い, 後ろが空白のみなら先頭."
   (interactive)
   (if (looking-back "^[ 　\t]+" -1000)
       (move-beginning-of-line nil)
     (back-to-indentation)))
 
 (defun smart-delete-whitespace-backward ()
-  "スペースだけではなく段階的に改行も削除する."
+  "スペースだけではなく段階的に改行も削除します."
   (interactive)
   (if (looking-back "\n" -1000)
       (delete-char -1)
@@ -26,7 +27,7 @@
       (replace-match ""))))
 
 (defun smart-delete-whitespace-forward ()
-  "スペースだけではなく段階的に改行も削除する."
+  "スペースだけではなく段階的に改行も削除します."
   (interactive)
   (if (looking-at "\n")
       (delete-char 1)
@@ -34,19 +35,19 @@
       (replace-match ""))))
 
 (defun delete-whitespace-backward ()
-  "前にある空白文字を全削除する."
+  "前にある空白文字を全削除します."
   (interactive)
   (when (looking-back "[[:blank:]\n]+" nil t)
     (replace-match "")))
 
 (defun delete-whitespace-forward ()
-  "後ろにある空白文字を全削除する."
+  "後ろにある空白文字を全削除します."
   (interactive)
   (when (looking-at "[[:blank:]\n]+")
     (replace-match "")))
 
 (defun isearch-exit-previous ()
-  "通常isearchは終了するとき, マッチした文字列の末尾に移動するが, これは先頭に移動する."
+  "通常isearchは終了するとき, マッチした文字列の末尾に移動しますが, これは先頭に移動します."
   (interactive)
   (let ((found-string-length (length isearch-string))
         (forward isearch-forward))
@@ -57,12 +58,13 @@
                    (+ (point) found-string-length))))))
 
 (defun indent-whole-buffer ()
+  "バッファ全体をインデントします."
   (interactive)
   (save-excursion
     (indent-region (point-min) (point-max))))
 
 (defun sort-dwim ()
-  "1行選択している時は単語のソート, 選択してない時はパラグラフの行ソート, 選択している時はその範囲の行ソート"
+  "1行選択している時は単語のソート, 選択してない時はパラグラフの行ソート, 選択している時はその範囲の行ソート."
   (interactive)
   (save-excursion
     (if (and (use-region-p)
@@ -76,44 +78,53 @@
              (sort-lines nil (region-beginning) (region-end))))))
 
 (defun sort-lines-whole-buffer ()
-  "バッファ全体をソートします"
+  "バッファ全体をソートします."
   (interactive)
   (save-excursion
     (sort-lines nil (point-min) (point-max))))
 
 (defun align-space (beg end)
+  "スペースを基準に整形します."
   (interactive (list (region-beginning) (region-end)))
   (align-regexp beg end "\\(\\s-*\\) " 1 0 t))
 
 (defun kill-region-or-word-at-point ()
+  "選択範囲か単語を切り取ります."
   (interactive)
   (apply 'kill-region (region-or-thing-at-point 'word)))
 
 (defun kill-region-or-sexp-at-point ()
+  "選択範囲か式を切り取ります."
   (interactive)
   (apply 'kill-region (region-or-thing-at-point 'sexp)))
 
 (defun kill-region-or-symbol-at-point ()
+  "選択範囲かシンボルを切り取ります."
   (interactive)
   (apply 'kill-region (region-or-thing-at-point 'symbol)))
 
 (defun kill-ring-save-region-or-word-at-point ()
+  "選択範囲か単語をコピーします."
   (interactive)
   (apply 'kill-ring-save (region-or-thing-at-point 'word)))
 
 (defun kill-ring-save-region-or-sexp-at-point ()
+  "選択範囲か式をコピーします."
   (interactive)
   (apply 'kill-ring-save (region-or-thing-at-point 'sexp)))
 
 (defun kill-ring-save-region-or-symbol-at-point ()
+  "選択範囲かシンボルをコピーします."
   (interactive)
   (apply 'kill-ring-save (region-or-thing-at-point 'symbol)))
 
 (defun copy-whole-line ()
+  "現在の行をコピーします."
   (interactive)
   (apply 'kill-ring-save (region-or-thing-at-point 'line)))
 
 (defun region-or-thing-at-point (thing)
+  "選択範囲か `THING' を取得します."
   (if (use-region-p)
       (list (region-beginning) (region-end))
     (let ((b (bounds-of-thing-at-point thing)))
@@ -139,11 +150,13 @@
      (set-mark d))))
 
 (defun kill-ring-save-whole ()
+  "バッファ全体をコピーします."
   (interactive)
   (save-excursion
     (kill-ring-save (buffer-end 0) (buffer-end 1))))
 
 (defun copy-to-register-@ (start end &optional delete-flag region)
+  "レジスタ@に選択範囲をコピーします."
   (interactive
    (list
     (region-beginning)
@@ -153,23 +166,28 @@
   (copy-to-register ?@ start end delete-flag region))
 
 (defun yank-register-@ ()
+  "レジスタ@から文字列を貼り付けます."
   (interactive)
   (insert-register ?@)
   (goto-char (prog1 (mark t) (set-marker (mark-marker) (point) (current-buffer)))))
 
 (defun scroll-down-one ()
+  "一行下にスクロールします."
   (interactive)
   (scroll-down 1))
 
 (defun scroll-up-one ()
+  "一行上にスクロールします."
   (interactive)
   (scroll-up 1))
 
 (defun kill-all-buffers ()
+  "バッファを全て閉じます."
   (interactive)
   (mapc 'kill-buffer (buffer-list)))
 
 (defun kill-file-or-dired-buffers ()
+  "ファイルとディレクトリのバッファを全て閉じます."
   (interactive)
   (mapc 'kill-buffer
         (cl-remove-if-not
@@ -180,37 +198,44 @@
          (buffer-list))))
 
 (defun buffer-dired-p (buffer)
+  "現在のバッファがdiredか."
   (eq (buffer-local-value 'major-mode buffer) 'dired-mode))
 
 (defun text-scale-reset ()
+  "拡大縮小をリセットします."
   (interactive)
   (text-scale-set 0))
 
 (defun other-window-fallback-split (COUNT &optional ALL-FRAMES)
+  "次のウィンドウに移動するか新しいウィンドウを作成します."
   (interactive "p")
   (when (one-window-p) (split-window-dwim))
   (other-window COUNT ALL-FRAMES))
 
 (defun other-window-backward ()
+  "後ろのウインドウに移動します."
   (interactive)
   (other-window -1))
 
 (defun split-window-dwim ()
+  "適当な位置に新しいウィンドウを作ります."
   (interactive)
   (split-window nil nil (suggest-window-locate)))
 
 (defun split-window-dwim-and-other ()
+  "ウインドウを作った後にそのウインドウに移動します."
   (interactive)
   (split-window nil nil (suggest-window-locate))
   (other-window 1))
 
 (defun suggest-window-locate ()
+  "新しいウィンドウを開くべき位置をサジェストします."
   (if (< (window-pixel-width) (window-pixel-height))
       'below
     'right))
 
 (defun revert-buffer-safe-confirm ()
-  "更新されていないファイルなら確認をしないrevert-buffer"
+  "更新されていないファイルなら確認をしないrevert-buffer."
   (interactive)
   (revert-buffer nil (not (buffer-modified-p))))
 
